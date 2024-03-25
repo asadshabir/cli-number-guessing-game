551a32eb936c6595e81eeb89f18607c5e8169880
asadshabir <asadshabir505@gmail.com>
2024-03-26 03:52:36 +0500

    created number guessing game

---
+++ main.ts
@@ -0,0 +1,24 @@
+const randomNumber = 5;
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+
+const answers = await inquirer.prompt([
+    
+    {
+        name : "numberGuessingGame",
+        type : "number",
+        message : "Guess any rendom number :"
+
+    }
+])
+
+if (answers.numberGuessingGame === randomNumber ) {
+    console.log(chalk.blue.bold("Your selected number is true "));
+    
+    
+} else {
+    console.log(chalk.red.bold.italic("You have guessed the wrong number !!"));
+    
+    
+}
